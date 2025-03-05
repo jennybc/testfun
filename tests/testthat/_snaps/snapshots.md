8d84efc7e4ac6a1d5ef0e7decd0dcf52c8d9754e
Jenny Bryan <jenny.f.bryan@gmail.com>
2025-03-04 19:45:16 -0800

    Add snapshot test for testthat reprex

---
+++ tests/testthat/_snaps/snapshots.md
@@ -0,0 +1,15 @@
+# snapshot test 1
+
+    Code
+      # first snapshot
+
+# snapshot test 2
+
+    Code
+      # second snapshot
+
+# snapshot test 3
+
+    Code
+      # third snapshot
+
---
+++ tests/testthat/test-snapshots.R
@@ -0,0 +1,11 @@
+test_that("snapshot test 1", {
+  expect_snapshot("first snapshot")
+})
+
+test_that("snapshot test 2", {
+  expect_snapshot("second snapshot")
+})
+
+test_that("snapshot test 3", {
+  expect_snapshot("third snapshot")
+})
